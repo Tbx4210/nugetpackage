6a60eda5a6237e7d3e74c1147ce4ad940cdf9be7
Tbx4210 <132796635+Tbx4210@users.noreply.github.com>
2024-12-12 01:25:29 -0500

    nuget package final commit

---
+++ ClassLibrary1/ClassLibrary1Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public void Log(string text)
+        {
+            Console.WriteLine(text);
+            
+        }
+    }
+}
